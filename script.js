0460d635307745555a1d629dedd6c6fdd8821fce
iMythms <maythams.149@gmail.com>
2024-09-29 18:20:15 +0300

    addjusting root

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let board, turn, winner, tie
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = () => {
+	return document.querySelectorAll('.sqr')
+}
+const messageEl = () => {
+	return document.querySelectorAll('.message')
+}
+
 /*-------------------------------- Functions --------------------------------*/
+
+const init = () => {
+	console.log('will use this later')
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
